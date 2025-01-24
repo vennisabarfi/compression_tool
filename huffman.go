895a024f98b25bf2b9df05672553b1d033f7f3d1
unknown <vennisabarfi@gmail.com>
2025-01-23 23:22:29 -0500

    still working. update to switch devices tomorrow

--- huffman.go
+++ huffman.go
@@ -1,13 +1,34 @@
 package main
 
-// huffmantree struct
-type huffmanTree struct {
-	nodes    []huffmanNode
-	nextNode int
+// Node struct
+type Node struct {
+	char  rune
+	count int
+	left  *Node
+	right *Node
 }
 
-// struct to hold a node in the tree
-type huffmanNode struct {
-	left, right           uint16
-	leftValue, rightValue uint16
+// https://reintech.io/blog/comprehensive-guide-huffman-coding-algorithm-go
+
+//  implementing algo by creating a priority queue. Ordering queue according to the frequency of the characters
+type PriorityQueue []*Node
+
+//  length of queue
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].count < pq[j].count
+}
+
+// swap
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+// push on to heap
+func (pq PriorityQueue) Push(x interface{}) {
+
+	pq = append(pq, x.(*Node))
 }
